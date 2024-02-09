c0cfc41038d32a4344ed1474b3386a5220652015
Dasharath Jumanal <v.dasharath.jumanal@accenture.com>
2024-02-09 19:34:00 +0530

    episode -1

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement('div',{id:"parent"},
+React.createElement('h1',{},"Hello World from React"));
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(heading);
